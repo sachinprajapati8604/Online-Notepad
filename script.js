7e8a6b64481236c4be5f4e62e5260cb5995c9d9e
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-07-23 16:54:29 +0530

    light and dark mode toggle added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function saveNotes() {
     let feature_flag = document.getElementById('feature-flag');
     let text_content = document.getElementById('textarea').value;
@@ -21,6 +20,8 @@ function saveNotes() {
         // console.log("unchecked")
 
     }
+
+    
     // console.log(text_content);
 
 }
@@ -49,30 +50,48 @@ function reset() {
 function deleteNotes() {
     let data = localStorage.getItem('autosave-data');
     let output_box = document.getElementById('output-box');
-    if(data==="" || data===null){
+    if (data === "" || data === null) {
         swal("Nothing to be delete", "first write something and save it.");
 
-    }else{
+    } else {
 
-    swal({
-        title: "Are you sure?",
-        text: "Once deleted, you will not be able to recover this note!",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-    })
-        .then((willDelete) => {
-            if (willDelete) {
-                localStorage.removeItem('autosave-data');
-                output_box.style.display = 'none';
-                swal("Poof! Your note has been deleted!", {
-                    icon: "success",
-                });
-            } else {
-                swal("Your note is safe!");
-            }
-        });
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this note!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    localStorage.removeItem('autosave-data');
+                    output_box.style.display = 'none';
+                    swal("Poof! Your note has been deleted!", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("Your note is safe!");
+                }
+            });
     }
 
+}
+
+function changeMode() {
+    let parent = document.getElementById('container');
+    let mode_icon = document.getElementById('mode-icon');
+    if (parent.className === 'container') {
+        parent.className = 'container night'
+        document.body.style.background = '#202124';
+        mode_icon.className = 'fas fa-moon'
+        mode_icon.style.background = '#00002c'
+        mode_icon.style.color = '#fff'
+    } else {
+        parent.className = 'container';
+        document.body.style.background = '#fff';
+        mode_icon.className = 'fas fa-sun'
+        mode_icon.style.background = '#fff'
+        mode_icon.style.color = '#000'
+    }
 }
 
